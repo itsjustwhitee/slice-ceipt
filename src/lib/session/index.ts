8fcbadb7f05f9974c15168cf94b4147a666e3c32
itsjustwhitee <matteofontolan215@gmail.com>
2026-07-17 22:43:20 +0200

    feat: add session layer barrel export

---
+++ src/lib/session/index.ts
@@ -0,0 +1,12 @@
+export {
+	createGroupItemsFromParsed,
+	computeGroupTotals,
+	type GroupItem,
+	type GroupUnitState
+} from './group';
+export {
+	createSingleItemsFromParsed,
+	computeSingleTotal,
+	type SingleItem,
+	type SingleUnitState
+} from './single';
