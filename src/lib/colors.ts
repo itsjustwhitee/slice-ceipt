cdc1e6700c2989579f0e16870dafbd919e83287f
itsjustwhitee <matteofontolan215@gmail.com>
2026-07-19 17:39:37 +0200

    feat: :lipstick: Extend color palette for users

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,14 +1,23 @@
 export const PARTICIPANT_COLORS = [
-	'#1c8dd4',
-	'#34d7d4',
-	'#96de21',
+	'#1c94de',
+	'#2daaa8',
+	'#7dae30',
 	'#cb2b86',
-	'#f8c51f',
-	'#911fe2',
+	'#b8941e',
+	'#a13ee8',
 	'#d52941',
-	'#d52eff',
+	'#ee2eff',
 	'#ff8800',
-	'#0008ff'
+	'#4046ee',
+	'#52909e',
+	'#2daaa8',
+	'#469a40',
+	'#ad5b8a',
+	'#5f532c',
+	'#7334a1',
+	'#ae2121',
+	'#ff4000',
+	'#487783'
 ];
 
 export function participantColor(index: number): string {
